21f81858173ba689343b7b65ca97fa038b70c5a4
stormytuna <stormytuna@outlook.com>
2025-10-17 22:10:19 +0100

    update fuzzel config

--- home/features/programs/fuzzel.nix
+++ home/features/programs/fuzzel.nix
@@ -9,9 +9,15 @@
     settings = {
       main = {
         font = lib.mkForce "${config.stylix.fonts.monospace.name}:size=${toString (config.stylix.fonts.sizes.popups * 1.5)}";
-        width = 40;
-        lines = 8;
-        inner-pad = 1;
+        width = 50;
+        lines = 12;
+        horizontal-pad = 4;
+        vertical-pad = 4;
+        inner-pad = 4;
+      };
+      border = {
+        width = 2;
+        radius = 0;
       };
     };
   };
