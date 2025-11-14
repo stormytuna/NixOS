fc39a9e450a4b6d58401e666cb3c68eb85ed4b1e
stormytuna <stormytuna@outlook.com>
2025-11-14 11:27:21 +0000

    update git config to use gnome-keyring

--- features/programs/git.nix
+++ features/programs/git.nix
@@ -1,8 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   programs.git = {
     enable = true;
     # TODO: Finish
     config = {
+      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
     };
   };
 }
